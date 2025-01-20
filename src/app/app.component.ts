4233cda4c1d776216c40484a54f6469f7f6287d9
sandranymark <sandra@nymark.nu>
2025-01-20 22:31:05 +0100

    Wishlist-angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wishItem';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  providers: [],
 })
 export class AppComponent {
-  title = 'wishllist';
+  items: WishItem[] = [
+    new WishItem('I want to learn Angular'),
+    new WishItem('Get Coffee', true),
+    new WishItem('Go to the bathroom'),
+  ];
+
+  title = 'My wishllist';
+
+  newWishText = '';
+
+  addNewWish() {
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
+  }
+
+  toggleItem(item: WishItem) {
+    item.isComplete = !item.isComplete;
+    console.log(item);
+  }
 }
